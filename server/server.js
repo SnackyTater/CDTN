77f10d62fbcb4061236f368614e1aa9ce44e5ab2
SnackyTater <taterazay98@gmail.com>
2021-08-30 17:55:43 +0700

    sth

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({path: path.resolve(__dirname, '..','.env')});
 
 const mongoose = require('./config/mongoose');
 const routerConfig = require('./config/route');
@@ -14,14 +14,13 @@ mongoose();
 app.use(cors());
 app.use(express.json());
 
+//include API routes
+app.use('/', routerConfig)
+
 app.use(express.static(path.resolve(__dirname, '..', 'client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 })
-
-
-//include API routes
-app.use('/', routerConfig)
 
 //listen request
 app.listen(process.env.PORT || 5000, () => {
