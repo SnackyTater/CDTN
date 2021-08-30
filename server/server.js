c5154c777e7571a90f6750154a342a6fb8b5d271
SnackyTater <taterazay98@gmail.com>
2021-08-30 14:22:32 +0700

    sth aaaaaaaaaaaaa

--- server/server.js
+++ server/server.js
@@ -14,12 +14,6 @@ mongoose();
 app.use(cors());
 app.use(express.json());
 
-//setup for frontend response
-app.use(express.static(path.resolve(__dirname,'..','client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-})
-
 //include API routes
 app.use('/', routerConfig)
 
