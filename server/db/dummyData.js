7419e67668fde49bb189a3065a093e99bf744b8b
SnackyTater <taterazay98@gmail.com>
2021-09-05 09:29:06 +0700

    minor change to all controller

---
+++ server/db/dummyData.js
@@ -0,0 +1,55 @@
+const user = [{
+    accountInfo: {
+        username: "tater-nope",
+        email: "taterazay98@gmail.com",
+        password: "tater123",
+    },
+    userInfo: {
+        profileImage: {
+            type: String,
+            default: '',
+        },
+        fullName: "Snacky Tater",
+        DateOfBirth: {
+            type: Date,
+        },
+        gender: "male",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    }
+}, {
+    accountInfo: {
+        username: "tater-like",
+        email: "taterazay98@gmail.com",
+        password: "tater123",
+    },
+    userInfo: {
+        profileImage: {
+            type: String,
+            default: '',
+        },
+        fullName: "like",
+        DateOfBirth: {
+            type: Date,
+        },
+        gender: "male",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    }
+}, {
+    accountInfo: {
+        username: "tater-block",
+        email: "taterazay98@gmail.com",
+        password: "tater123",
+    },
+    userInfo: {
+        profileImage: {
+            type: String,
+            default: '',
+        },
+        fullName: "block",
+        DateOfBirth: {
+            type: Date,
+        },
+        gender: "male",
+        description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    }
+}]
